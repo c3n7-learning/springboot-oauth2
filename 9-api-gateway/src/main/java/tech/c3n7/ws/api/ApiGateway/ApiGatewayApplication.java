9904e7bf7bfdc076c292bcfae5de38aef9fc4108
Timothy Karani <kartimothy@gmail.com>
2022-08-28 15:53:12 +0300

    Added api gateway

---
+++ 9-api-gateway/src/main/java/tech/c3n7/ws/api/ApiGateway/ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package tech.c3n7.ws.api.ApiGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
