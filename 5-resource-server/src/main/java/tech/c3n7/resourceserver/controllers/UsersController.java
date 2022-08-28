75b362f9c6e0eee0fb8e86c95698636230081c4b
Timothy Karani <kartimothy@gmail.com>
2022-08-28 15:28:17 +0300

    Can post authorize

--- 5-resource-server/src/main/java/tech/c3n7/resourceserver/controllers/UsersController.java
+++ 5-resource-server/src/main/java/tech/c3n7/resourceserver/controllers/UsersController.java
@@ -1,10 +1,12 @@
 package tech.c3n7.resourceserver.controllers;
 
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
+import tech.c3n7.resourceserver.response.UserRest;
 
 @RestController
 @RequestMapping("/users")
@@ -21,5 +23,11 @@ public class UsersController {
     @DeleteMapping(path="/{id}")
     public String deleteUser(@PathVariable String id, @AuthenticationPrincipal Jwt jwt) {
         return "Deleted user with id " + id + " and jwt subject " + jwt.getSubject();
+    }
+
+    @PostAuthorize("returnObject.userId == #jwt.subject")
+    @GetMapping(path="/{id}")
+    public UserRest getUser(@PathVariable String id, @AuthenticationPrincipal Jwt jwt) {
+        return new UserRest("Timothy", "Karani", "e7363548-cd0e-49ea-8d6e-40fc8f59b93e");
     }
 }
