fd43f25da253821765d51c4b458973a07fbc7fec
Timothy Karani <kartimothy@gmail.com>
2022-09-11 10:38:19 +0300

    First of many

---
+++ 12-PhotoAppWebClient/src/test/java/tech/c3n7/ws/clients/ClientsApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.c3n7.ws.clients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
