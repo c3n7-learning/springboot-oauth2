3c130e37becaf6f350e5f135ffb607ec6bd60fc7
Timothy Karani <kartimothy@gmail.com>
2022-09-13 18:02:17 +0300

    Added html page

--- 14-SocialWebClient/src/main/java/tech/c3n7/clients/sociallogin/SocialWebClient/controllers/HomePageController.java
+++ 14-SocialWebClient/src/main/java/tech/c3n7/clients/sociallogin/SocialWebClient/controllers/HomePageController.java
@@ -1,4 +1,23 @@
 package tech.c3n7.clients.sociallogin.SocialWebClient.controllers;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.parameters.P;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomePageController {
+
+    @GetMapping("/home")
+    public String displayHomePage(Model model, @AuthenticationPrincipal OAuth2User principal) {
+
+        if(principal != null) {
+            String name = principal.getAttribute("name");
+            model.addAttribute("name", name);
+        }
+
+        return "home";
+    }
 }
