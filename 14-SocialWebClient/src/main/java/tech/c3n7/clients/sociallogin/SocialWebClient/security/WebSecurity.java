2563a0c2bd3dae4c1cae997fb03b9bfa3e421f51
Timothy Karani <kartimothy@gmail.com>
2022-09-13 18:11:40 +0300

    Added oauth config

---
+++ 14-SocialWebClient/src/main/java/tech/c3n7/clients/sociallogin/SocialWebClient/security/WebSecurity.java
@@ -0,0 +1,17 @@
+package tech.c3n7.clients.sociallogin.SocialWebClient.security;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@SuppressWarnings("deprecation")
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .oauth2Login();
+    }
+}
