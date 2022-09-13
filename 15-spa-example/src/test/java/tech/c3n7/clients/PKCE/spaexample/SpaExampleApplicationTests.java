91ff4aee860779cae65ddaf9d17d60e5216f8fea
Timothy Karani <kartimothy@gmail.com>
2022-09-13 22:11:08 +0300

    first of many

---
+++ 15-spa-example/src/test/java/tech/c3n7/clients/PKCE/spaexample/SpaExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.c3n7.clients.PKCE.spaexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
