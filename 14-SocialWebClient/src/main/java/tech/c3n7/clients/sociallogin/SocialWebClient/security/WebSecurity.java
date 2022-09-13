cac7f7ba9651d09cd1526ba2bfe0c824a000fb80
Timothy Karani <kartimothy@gmail.com>
2022-09-13 21:12:03 +0300

    Added company URL

--- 14-SocialWebClient/src/main/java/tech/c3n7/clients/sociallogin/SocialWebClient/security/WebSecurity.java
+++ 14-SocialWebClient/src/main/java/tech/c3n7/clients/sociallogin/SocialWebClient/security/WebSecurity.java
@@ -1,15 +1,19 @@
 package tech.c3n7.clients.sociallogin.SocialWebClient.security;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SuppressWarnings("deprecation")
+@EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/").permitAll()
+                .and()
+                .authorizeRequests()
                 .anyRequest().authenticated()
                 .and()
                 .oauth2Login();
