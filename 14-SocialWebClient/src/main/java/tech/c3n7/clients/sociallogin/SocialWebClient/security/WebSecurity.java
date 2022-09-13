5a3ed6cbf4eefbfda1cb29f89533748d4e672d62
Timothy Karani <kartimothy@gmail.com>
2022-09-13 21:37:59 +0300

    Added log out handler

--- 14-SocialWebClient/src/main/java/tech/c3n7/clients/sociallogin/SocialWebClient/security/WebSecurity.java
+++ 14-SocialWebClient/src/main/java/tech/c3n7/clients/sociallogin/SocialWebClient/security/WebSecurity.java
@@ -1,12 +1,18 @@
 package tech.c3n7.clients.sociallogin.SocialWebClient.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 
 @SuppressWarnings("deprecation")
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    ClientRegistrationRepository clientRegistrationRepository;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -19,9 +25,19 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
                     .oauth2Login()
                 .and()
                     .logout()
-                    .logoutSuccessUrl("/")
+//                    .logoutSuccessUrl("/")
+                    .logoutSuccessHandler(oidcClientInitiatedLogoutSuccessHandler())
                     .invalidateHttpSession(true)
                     .clearAuthentication(true)
                     .deleteCookies("JSESSIONID");
+    }
+
+    private OidcClientInitiatedLogoutSuccessHandler oidcClientInitiatedLogoutSuccessHandler() {
+        // clientRegistrationRepository will have the open-id configuration and thus the logout url for the openID oauth service
+        // doing the heavy lifting for us
+        OidcClientInitiatedLogoutSuccessHandler successHandler =
+                new OidcClientInitiatedLogoutSuccessHandler(clientRegistrationRepository);
+        successHandler.setPostLogoutRedirectUri("http://localhost:8080/");
+        return successHandler;
     }
 }
