64c6f0834de5022339f000fe966e8847b12c4451
Timothy Karani <kartimothy@gmail.com>
2022-09-14 08:30:36 +0300

    Added cors configs

--- 11.1-resource-server-eureka/src/main/java/tech/c3n7/resourceserver/security/WebSecurity.java
+++ 11.1-resource-server-eureka/src/main/java/tech/c3n7/resourceserver/security/WebSecurity.java
@@ -1,5 +1,6 @@
 package tech.c3n7.resourceserver.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -8,6 +9,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @SuppressWarnings("deprecation")
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
@@ -20,7 +26,8 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
         JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
         jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeyCloakRoleConverter());
 
-        http.authorizeRequests()
+        http.cors().and()
+                .authorizeRequests()
                 .antMatchers(HttpMethod.GET, "/users/status/check")
 //                .hasAuthority("SCOPE_profile")
 //                .hasRole("developer")
@@ -30,4 +37,18 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
                 .oauth2ResourceServer().jwt()
                 .jwtAuthenticationConverter(jwtAuthenticationConverter);
     }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOrigins(Arrays.asList("*"));
+        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST"));
+        corsConfiguration.setAllowedHeaders(Arrays.asList("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        return source;
+    }
+
 }
