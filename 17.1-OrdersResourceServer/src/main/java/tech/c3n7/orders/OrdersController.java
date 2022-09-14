bb54177d02f8cd3746babf5f1cab50cf07a5136b
Timothy Karani <kartimothy@gmail.com>
2022-09-14 19:33:41 +0300

    Can auth check

--- 17.1-OrdersResourceServer/src/main/java/tech/c3n7/orders/OrdersController.java
+++ 17.1-OrdersResourceServer/src/main/java/tech/c3n7/orders/OrdersController.java
@@ -1,5 +1,6 @@
 package tech.c3n7.orders;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
@@ -8,6 +9,7 @@ import java.util.UUID;
 
 @RestController
 public class OrdersController {
+    @GetMapping("/orders")
     public List<OrderRest> getOrders() {
         OrderRest order1 = new OrderRest(
                 UUID.randomUUID().toString(),
