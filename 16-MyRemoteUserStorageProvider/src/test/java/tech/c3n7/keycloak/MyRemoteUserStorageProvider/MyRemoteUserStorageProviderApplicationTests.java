8d9b874b1d0bbec5fe1e493b2e128673412777e0
Timothy Karani <kartimothy@gmail.com>
2022-09-14 08:48:16 +0300

    First of many

---
+++ 16-MyRemoteUserStorageProvider/src/test/java/tech/c3n7/keycloak/MyRemoteUserStorageProvider/MyRemoteUserStorageProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.c3n7.keycloak.MyRemoteUserStorageProvider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyRemoteUserStorageProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
